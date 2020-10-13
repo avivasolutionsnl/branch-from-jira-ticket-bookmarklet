56bc70aa2a25d68855283116253a8f29af955acf
timvanschie1 <timvanschie1@gmail.com>
2020-10-13 11:16:15 +0200

    Added first version with Readme text.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # branch-from-jira-ticket-bookmarket
-Bookmarklet to create a branch from a Jira ticket
+Bookmarklet to create a feature branch from a Jira ticket
+
+1. In Chrome, go to your Bookmarklets (chrome://bookmarks/) and add a new one.
+2. Copy the code from bookmarklet-code.js into the URL field and give it a proper name.
+3. When a Jira ticket is opened (in a dialog view or on the detail page), click on the Bookmarklet.
+4. The code to create the branch is copied to you clipboard.
+5. Go to the terminal and paste the copied code. The branch is directly created and pushed to the server.  
+
+Of course, you can adjust the bookmarklet code to fit your needs.
---
+++ bookmarket-code.js
@@ -0,0 +1,31 @@
+javascript:(function () {
+    const contextEl = document.querySelector("[role='dialog']") || document.querySelector("body");
+    const ticketNrEl = contextEl.querySelector("[class*=BreadcrumbsContainer-] > div:last-child a");
+    const titleEl = contextEl.querySelector("h1");
+
+    function showMessage(message) {
+        const el = document.createElement('div');
+        el.innerText = message;
+        el.style = "position:fixed; top: 0; right: 0; left: 0; padding: 10px; text-align: center; background-color: rgba(0,0,0,0.7); color: white; z-index: 9999;";
+        document.body.appendChild(el);
+        setTimeout(() => {
+            document.body.removeChild(el);
+        }, 3000);
+    }
+
+    if (!ticketNrEl || !titleEl) {
+        showMessage("Something went wrong. Do you have an open Jira ticket on the page?");
+        return;
+    }
+
+    const titleDashedLowercase = titleEl.innerText.replace(/\s+/g, '-').toLowerCase();
+    const ticketNr = ticketNrEl.innerText;
+    const createBranchString =
+        `git checkout -b ${ticketNr}/${titleDashedLowercase} \ngit push -u origin ${ticketNr}/${titleDashedLowercase}\n`;
+
+    navigator.clipboard.writeText(createBranchString)
+        .then(() => showMessage(`Copied:\n ${createBranchString}`))
+        .catch((error) => {
+            alert(`Copying failed. ${error}`)
+        })
+})();
